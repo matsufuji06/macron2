e52271db588027e12b9be09b1127999ea0652e50
matsufuji06 <n.matsufuji06@gmail.com>
2022-07-06 22:12:18 +0900

    PostControllerに表示する食べ物のダミーデータを入力

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -6,5 +6,61 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        // ダミーデータ
+        $posts = [
+            (object) [
+                'id' => 1,
+                'food' => 'ラーメン１',
+                'image' => '',
+                'calorie' => 1000,
+                'carbo' => 1000,
+                'fat' => 1000,
+                'protein' => 1000,
+                'weight' => 1000,
+                'date' => now(),
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 1,
+                    'name' => 'ユーザー名１',
+                ],
+            ],
+            (object) [
+                'id' => 2,
+                'food' => 'ラーメン２',
+                'image' => '',
+                'calorie' => 1000,
+                'carbo' => 1000,
+                'fat' => 1000,
+                'protein' => 1000,
+                'weight' => 1000,
+                'date' => now(),
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 2,
+                    'name' => 'ユーザー名２',
+                ],
+            ],
+            (object) [
+                'id' => 3,
+                'food' => 'ラーメン３',
+                'image' => '',
+                'calorie' => 1000,
+                'carbo' => 1000,
+                'fat' => 1000,
+                'protein' => 1000,
+                'weight' => 1000,
+                'date' => now(),
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 3,
+                    'name' => 'ユーザー名３',
+                ],
+            ]
+            
+        ];
+
+        return view('posts.index', ['posts' => $posts]);
+    }
 }
