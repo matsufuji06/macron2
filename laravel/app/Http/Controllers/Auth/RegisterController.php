8d159c09537c9c1c200a299f30ae2a88d304cf8f
matsufuji06 <n.matsufuji06@gmail.com>
2022-07-07 23:00:20 +0900

    ナビバーにログイン・ログアウト画面を出し分け

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'alpha_num', 'max:255', 'min:3', 'max:16', 'unique:users'],
+            'name' => ['required', 'string', 'alpha_num', 'max:16', 'min:3', 'max:16', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
--- laravel/config/database.php
+++ laravel/config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'macron2'),
+            'database' => env('DB_DATABASE', 'default'),
             'username' => env('DB_USERNAME', 'default'),
             'password' => env('DB_PASSWORD', 'secret'),
             'charset' => 'utf8',
