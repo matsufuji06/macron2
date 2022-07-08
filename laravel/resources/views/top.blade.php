359f0b7b24d2ea1a13c56041488279674363db9d
matsufuji06 <n.matsufuji06@gmail.com>
2022-07-08 10:22:25 +0900

    トップページを一時的に作成

---
+++ laravel/resources/views/top.blade.php
@@ -0,0 +1,6 @@
+@extends('app')
+
+@section('title', '記事一覧')
+
+@include('nav')
+<p>トップページです。後日更新予定です。</p>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('top');
+});
 
 Auth::routes();
 Route::get('/post', 'PostController@index');
