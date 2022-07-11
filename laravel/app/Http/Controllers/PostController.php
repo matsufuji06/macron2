9bd652489dca5730e14ab104651d21b2fc6765b3
matsufuji06 <n.matsufuji06@gmail.com>
2022-07-11 10:05:24 +0900

    記事一覧からの編集＆削除画面への遷移を実装

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -36,7 +36,7 @@ class PostController extends Controller
     }
 
     public function edit(Post $post) {
-        return view('post.edit', ['post' => $post]);
-        
+        return view('posts.edit', ['post' => $post]);
+
     }
 }
--- laravel/resources/views/posts/index.blade.php
+++ laravel/resources/views/posts/index.blade.php
@@ -17,6 +17,55 @@
            {{ $post->created_at->format('Y/m/d H:i') }}
           </div>
         </div>
+
+        @if( Auth::id() === $post->user_id )
+          <!-- 記事更新と削除のdropdown -->
+          <div class="ml-auto card-text">
+            <div class="dropdown">
+              <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button type="button" class="btn btn-link text-muted m-0 p-2">
+                  <i class="fas fa-ellipsis-v"></i>
+                </button>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item" href="{{ route("posts.edit", ["post" => $post]) }}">
+                  <i class="fas fa-pen mr-1"></i>記事を更新する
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $post->id }}">
+                  <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+                </a>
+              </div>
+            </div>
+          </div>
+          <!--  記事更新と削除のdropdown -->
+  
+          <!-- 記事削除のmodal -->
+          <div id="modal-delete-{{ $post->id }}" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <form method="POST" action="{{ route('posts.destroy', ['post' => $post]) }}">
+                  @csrf
+                  @method('DELETE')
+                  <div class="modal-body">
+                    {{ $post->food }}を削除します。よろしいですか？
+                  </div>
+                  <div class="modal-footer justify-content-between">
+                    <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                    <button type="submit" class="btn btn-danger">削除する</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <!-- 記事削除のmodal -->
+        @endif
+
       </div>
       <div class="card-body pt-0 pb-2">
         <h3 class="h4 card-title">
