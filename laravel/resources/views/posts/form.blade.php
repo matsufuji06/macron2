36c02a574b380178aeafaeec7055829f77c8b2d5
matsufuji06 <n.matsufuji06@gmail.com>
2022-07-11 10:10:09 +0900

    Null合体演算子(??)でメニュー記事編集画面遷移時に保存済みの情報を保持

--- laravel/resources/views/posts/form.blade.php
+++ laravel/resources/views/posts/form.blade.php
@@ -1,36 +1,36 @@
 @csrf
 <div class="md-form">
   <label>食べたもの</label>
-  <input type="text" name="food" class="form-control" required value="{{ old('food') }}">
+  <input type="text" name="food" class="form-control" required value="{{ $post->food ?? old('food') }}">
 </div>
 
 <!-- ↓画像 -->
 <div class="form-group">
   <label></label>
-  <input type="file" name="image" class="form-control" required value="{{ old('image') }}">
+  <input type="file" name="image" class="form-control" required value="{{ $post->image ?? old('image') }}">
 </div>
 
 <div class="form-group">
   <label>炭水化物</label>
-  <input type="number" step="0.1" name="carbo" class="form-control" required value="{{ old('carbo') }}">
+  <input type="number" step="0.1" name="carbo" class="form-control" required value="{{ $post->carbo ?? old('carbo') }}">
 </div>
 
 <div class="form-group">
   <label>脂質</label>
-  <input type="number" step="0.1" name="fat" class="form-control" required value="{{ old('fat') }}">
+  <input type="number" step="0.1" name="fat" class="form-control" required value="{{ $post->fat ?? old('fat') }}">
 </div>
 
 <div class="form-group">
   <label>タンパク質</label>
-  <input type="number" step="0.1" name="protein" class="form-control" required value="{{ old('protein') }}">
+  <input type="number" step="0.1" name="protein" class="form-control" required value="{{ $post->protein ?? old('protein') }}">
 </div>
 
 <div class="form-group">
   <label>カロリー</label>
-  <input type="number" step="0.1" name="calorie" class="form-control" required value="{{ old('calorie') }}">
+  <input type="number" step="0.1" name="calorie" class="form-control" required value="{{ $post->calorie ?? old('calorie') }}">
 </div>
 
 <div class="form-group">
   <label>現在の体重</label>
-  <input type="number" step="0.1" name="weight" class="form-control" required value="{{ old('weight') }}">
+  <input type="number" step="0.1" name="weight" class="form-control" required value="{{ $post->weight ?? old('weight') }}">
 </div>
